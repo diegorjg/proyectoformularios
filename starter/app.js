6f2899fabd91547df3e543a0359df09f6548905b
diego <diegorjg@gmail.com>
2019-10-10 14:17:06 -0500

    first commit

---
+++ starter/app.js
@@ -0,0 +1,9 @@
+const shippingForm = document.querySelector('#crane-shipping-form');
+shippingForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  alert('ya casi terminamos!');
+  
+});
+
+
+
